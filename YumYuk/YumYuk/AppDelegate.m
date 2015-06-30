2e20af0aef40296e1cd3f9fed19f9f8fdb405810
Mauricio Narvaez <maunvz@stanford.edu>
2015-06-30 11:53:42 -0700

    asdfklhas

--- YumYuk/YumYuk/AppDelegate.m
+++ YumYuk/YumYuk/AppDelegate.m
@@ -39,7 +39,7 @@
     //TEMPORARY -- test that adds things to the server then downloads them and logs them
     dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
      //   [DatabaseAccess testDatabase];
-        [self downloadMPKEatsData];
+     //   [self downloadMPKEatsData];
     });
     
     // Create an item store
