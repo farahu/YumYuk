f46d25a4f1ced44a8f84a792a68c6efd66e74c26
nickiliang <kliang@kliang-mbp.dhcp.thefacebook.com>
2015-06-30 10:17:35 -0700

    Trying to add the individual restaurant page
    Loads, but crashes when you select a table row

---
+++ YumYuk/YumYuk/IndividualPageController.h
@@ -0,0 +1,15 @@
+//
+//  IndividualPageControllerTableViewController.h
+//  YumYuk
+//
+//  Created by Kristina Liang on 6/30/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class RestaurantList;
+
+@interface IndividualPageController : UITableViewController
+- (instancetype)initWithRestaurantList:(RestaurantList *)rest atIndex:(NSInteger)rIndex;
+
+@end
